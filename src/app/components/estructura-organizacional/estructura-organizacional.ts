e40c89b737d50902f1935634ec6e8f17e81a48db
Darwin Cuenca <darwincuenca1990@outlook.com>
2026-08-12 18:46:36 -0500

    Fotos del equipo de trabajo

--- src/app/components/estructura-organizacional/estructura-organizacional.ts
+++ src/app/components/estructura-organizacional/estructura-organizacional.ts
@@ -1,13 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-estructura-organizacional',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './estructura-organizacional.html',
   styleUrls: ['./estructura-organizacional.css'],
 })
 export class EstructuraOrganizacional {
+ director = {
+    nombre: 'Mgtr. Darwin Cuenca',
+    cargo: 'Dirección General',
+    foto: 'assets/images/estructuraOrganizacional/darwin-cuenca.jpg'
+  };
 
+  subdirectora = {
+    nombre: 'Ing. Viviana Cabrera',
+    cargo: 'Subdirectora General',
+    foto: 'assets/images/estructuraOrganizacional/Vivi.jpeg'
+  };
+
+  contabilidad = {
+    nombre: 'Mgtr. Daissy Alulima',
+    cargo: 'Contabilidad',
+    foto: 'assets/images/estructuraOrganizacional/daissy-alulima.jpg'
+  };
+
+  communityManager = {
+    nombre: 'Dayanna Tamay',
+    cargo: 'Community Manager',
+    foto: 'assets/images/estructuraOrganizacional/Daya.jpeg'
+  };
 }
